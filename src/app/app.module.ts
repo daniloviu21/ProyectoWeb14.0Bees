3582915fd8f71e5e94d78924b05b7316ca5195ad
daniloviu21 <daniel.darkwatch@gmail.com>
2025-02-05 18:40:42 -0600

    build: Crear proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,31 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { UserManagementComponent } from './pages/user-management/user-management.component';
+import { ProductManagementComponent } from './pages/product-management/product-management.component';
+import { OrderManagementComponent } from './pages/order-management/order-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    DashboardPageComponent,
+    UserManagementComponent,
+    ProductManagementComponent,
+    OrderManagementComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
