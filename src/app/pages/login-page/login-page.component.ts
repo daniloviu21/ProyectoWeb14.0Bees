fe3f2eb2d525c56114d4f8d785c9370012ebe823
David Pulido <146595522+DavidPulSor02@users.noreply.github.com>
2025-02-07 09:53:11 -0600

    mejore  estilo login

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: false,
-
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css',
-
+  styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  loginForm: FormGroup;
+  errorMessage: string = '';
 
+  // Credenciales estáticas
+  private validCredentials = {
+    email: 'admin@example.com',
+    password: '12345678'
+  };
+  router: any;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    const { email, password } = this.loginForm.value;
+
+    if (email === this.validCredentials.email && password === this.validCredentials.password) {
+      alert('Inicio de sesión exitoso 🎉');
+      this.router.navigate(['/home']);
+
+    } else {
+      this.errorMessage = 'Correo o contraseña incorrectos';
+    }
+  }
 }
