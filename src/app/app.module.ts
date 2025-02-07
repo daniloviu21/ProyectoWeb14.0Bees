8ffcb5fcd3738aee6444975e989e452ab8ac6678
David Pulido <146595522+DavidPulSor02@users.noreply.github.com>
2025-02-07 00:11:15 -0600

    Corregi problema app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,17 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { UserManagementComponent } from './pages/user-management/user-management.component';
 import { ProductManagementComponent } from './pages/product-management/product-management.component';
 import { OrderManagementComponent } from './pages/order-management/order-management.component';
-import { RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     RegisterPageComponent,
     DashboardPageComponent,
     UserManagementComponent,
@@ -25,9 +24,7 @@ import { LoginComponent } from './pages/login-page/login-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([]),
-    LoginComponent,
-
+    LoginComponent
   ],
   providers: [
     provideClientHydration(withEventReplay()),
