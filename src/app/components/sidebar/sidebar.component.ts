870f63a380cc2e6261ccb3d97efb02824db6e211
Tu Nombre <tuemail@example.com>
2025-02-07 10:27:57 -0600

    Resuelto el conflicto

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,9 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   standalone: false,
-  
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  public sidebarItems = [
+    { label: 'Inicio', icon: 'home', url: '/dashboard' },
+    {
+      label: 'Gestionar',
+      icon: 'add',
+      children: [
+        { label: 'Usuarios', icon: 'person', url: '/users' },
+        { label: 'Productos', icon: 'shopping_cart', url: '/products' },
+        { label: 'Almacén', icon: 'store', url: '/storage' }
+      ],
+      expanded: false // Estado para mostrar u ocultar el submenú
+    },
+    { label: 'Buscar', icon: 'search', url: '/search' }
+  ];
+
+  toggleSubmenu(item: any) {
+    if (item.children) {
+      item.expanded = !item.expanded;
+    }
+  }
 }
